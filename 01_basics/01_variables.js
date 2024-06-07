2ff769ea90034a937e59973f74153b8c3acc8044
Subhankar Sikdar <subhankarsikder500@gmail.com>
2024-06-07 06:44:13 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1001
+let accountEmail = "ulluck@gmail.com"
+var accountPassword = "abcd"
+accountCity = "kolkata"
+let accountState;
+
+// accountId = 50
+
+accountEmail = "12@gmail.com"
+accountPassword = "qwer"
+accountCity = "hyderabad"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
