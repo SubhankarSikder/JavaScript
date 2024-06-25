6d11e1355f3badacc92bba87358022cca11c18dc
Subhankar Sikdar <subhankarsikder500@gmail.com>
2024-06-25 04:46:11 +0000

    conversion of datatypes

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,73 @@
+/*
+example 1
+let score = "52abc"
+
+console.log(typeof score);
+
+let changeVariable = Number(score)
+console.log(typeof changeVariable);
+console.log(changeVariable);
+*/
+
+// example 2
+// let score = null
+
+// console.log(typeof score);
+
+// let changeVariable = Number(score)
+// console.log(typeof changeVariable);
+// console.log(changeVariable);
+
+
+// example 3
+// let score = "Tom"
+
+// console.log(typeof score);
+
+// let changeVariable = Number(score)
+// console.log(typeof changeVariable);
+// console.log(changeVariable);
+
+
+// example 4
+// let score = 45
+
+// console.log(typeof score);
+
+// let changeVariable = String(score)
+// console.log(typeof changeVariable);
+// console.log(changeVariable);
+
+
+// example 5
+// let score = "45"
+
+// console.log(typeof score);
+
+// let changeVariable = Boolean(score)
+// console.log(typeof changeVariable);
+// console.log(changeVariable);
+
+
+
+// example 6
+// let score = true
+
+// console.log(typeof score);
+// console.log(score);
+
+// let changeVariable = Number(score)
+// console.log(typeof changeVariable);
+// console.log(changeVariable);
+
+
+
+// example 6
+let score = "ulluck"
+
+console.log(typeof score);
+console.log(score);
+
+let changeVariable = Number(score)
+console.log(typeof changeVariable);
+console.log(changeVariable);
