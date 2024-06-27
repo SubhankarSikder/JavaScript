7c691f600d516b9cebce853affdbda25d905560b
Subhankar Sikdar <subhankarsikder500@gmail.com>
2024-06-27 05:37:51 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "Subhankar"
+
+const age = 26
+
+// console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const newName = new String("ulluck")
+
+console.log(name);
+console.log(newName);
