85523610c1f19a016a42791052d29a703cc08e0e
Subhankar Sikdar <subhankarsikder500@gmail.com>
2024-07-08 06:04:42 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+let myArray = [5, 4, 2, "ulluck", false ]
+// console.log(myArray[5]);
+
+let newArr1 = new Array(2,6,9,0,7,1)
+// console.log(newArr1);
+
+// console.log(newArr1.push(3));
+// console.log(newArr1);
+
+
+// newArr1.push(32)
+// console.log(newArr1);
+
+// newArr1.pop()
+// console.log(newArr1);
+
+// newArr1.unshift(5)
+// console.log(newArr1);
+
+// newArr1.shift()
+// console.log(newArr1);
+
+// newArr1.toLocaleString()
+// console.log(newArr1);
+
+// console.log(newArr1.includes(56)); 
+// console.log(newArr1.indexOf(2)); 
+
+
+let newArr2 = Array(newArr1.join())
+console.log(newArr1);
+console.log(newArr2)
+console.log(typeof newArr2)
