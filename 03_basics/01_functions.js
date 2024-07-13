b55f6aed655fcc26d8f5f0eef77276fc114bef19
Subhankar Sikdar <subhankarsikder500@gmail.com>
2024-07-13 04:40:47 +0000

    scope & functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,4 +22,37 @@ function hello(a,b=5){
     return a+b
 }
 const newHello = hello(22,10)
-console.log("newHello = ",newHello);
+// console.log("newHello = ",newHello);
+
+
+// rest/spread operator
+function cartPrice(...num1){
+    return num1
+}
+
+// console.log(cartPrice(200,500,300))
+
+
+const user = {
+    username: "ulluck",
+    prices: 250
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+// handleObject({
+//     username:"bholluck",
+//     price:350
+// })
+
+const newArray = [20,410,72]
+
+function returnSecondValue(anyarray){
+    return anyarray[1]
+}
+
+// console.log(returnSecondValue(newArray))
+console.log(returnSecondValue([20,1980,99]))
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+// var c=50
+// let a=199
+const b=370
+
+if(true){
+    let a =10
+    b =20
+    // console.log("inner : ",a);
+    console.log("inner : ",b);
+    // c=30
+}
+// console.log(a);
+console.log(b);
+// console.log(c);
