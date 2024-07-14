2ca6294ed37437e9438d8fbb3eb709624b62212d
Subhankar Sikdar <subhankarsikder500@gmail.com>
2024-07-14 14:06:17 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,11 +4,24 @@ const b=370
 
 if(true){
     let a =10
-    b =20
+    // b =20
     // console.log("inner : ",a);
-    console.log("inner : ",b);
+    // console.log("inner : ",b);
     // c=30
 }
 // console.log(a);
-console.log(b);
+// console.log(b);
 // console.log(c);
+
+// console.log(one(5))
+function one(value){
+    return value + 1
+}
+
+
+
+// console.log(two(5))
+const two = function(value){
+    return value + 2
+}
+
